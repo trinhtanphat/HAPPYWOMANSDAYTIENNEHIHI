783de362dde677d60475759e3e37afd958770d6e
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2022-03-07 22:51:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Gửi bạn thân<3';
+const giftUrl = 'https://www.facebook.com/tiennehihii';
+const eventName = "Happy Woman's Day!";
+const titleCard = 'Gửi bạn Tiên';
+const contentCard = "Happy Woman's Day. Wishing you a whole lot of happiness, today and for the rest of the year. Every home, every heart, every feeling, every moment of happiness is incomplete without you, only you can complete this world. Happy Women’s Day!";
+
+const giftImage = 'a1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
